4add1978f1390153b280ae1dd586b0e7e7a70c3c
caesarbriones43 <caesarbriones43@gmail.com>
2022-07-23 22:23:13 -0500

    modules basis & data types

---
+++ src/bases/01-types.ts
@@ -0,0 +1,10 @@
+export let name: string = "Cesar";
+export const age: number = 25;
+export const isValid: boolean = true;
+
+name = "Andrew";
+
+export const templateString = `String multiline
+1
+2
+3`;
